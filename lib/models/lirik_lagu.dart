974b9cf3192982bab4d37d588b675aaee42db918
Andri Fajar Sunandhar <andrirrf7@gmail.com>
2024-08-12 21:14:16 +0700

    first commit, app lirik lagu, Home Page

---
+++ lib/models/lirik_lagu.dart
@@ -0,0 +1,111 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class LirikLagu {
+  final int id;
+  final String namaBand;
+  final String judulLagu;
+  final String photo;
+  final String lirikLagu;
+  LirikLagu({
+    required this.id,
+    required this.namaBand,
+    required this.judulLagu,
+    required this.photo,
+    required this.lirikLagu,
+  });
+}
+
+final listLirikLagu = [
+  LirikLagu(
+    id: 1,
+    namaBand: 'Iwan Fals',
+    judulLagu: 'Ibu',
+    photo: 'https://i.scdn.co/image/ab67616d0000b2739eb700ca141b2a99b01fc100',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 2,
+    namaBand: 'Iwan Fals',
+    judulLagu: 'Pesawat Tempur',
+    photo: 'https://i.scdn.co/image/ab67616d0000b2739eb700ca141b2a99b01fc100',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 3,
+    namaBand: 'Iwan Fals',
+    judulLagu: 'Bento',
+    photo: 'https://i.scdn.co/image/ab67616d0000b2739eb700ca141b2a99b01fc100',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 4,
+    namaBand: 'Peterpan',
+    judulLagu: 'Menghapus Jejakmu',
+    photo:
+        'https://assets-a1.kompasiana.com/items/album/2020/12/24/foto-jadul-peterpan-5fe49b108ede4844d317cc22.jpg',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 5,
+    namaBand: 'Peterpan',
+    judulLagu: 'Ada Apa Denganmu',
+    photo:
+        'https://assets-a1.kompasiana.com/items/album/2020/12/24/foto-jadul-peterpan-5fe49b108ede4844d317cc22.jpg',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 6,
+    namaBand: 'Peterpan',
+    judulLagu: 'Semua Tentang Kita',
+    photo:
+        'https://assets-a1.kompasiana.com/items/album/2020/12/24/foto-jadul-peterpan-5fe49b108ede4844d317cc22.jpg',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 7,
+    namaBand: 'Sheila On 7',
+    judulLagu: 'Shepia',
+    photo: 'https://upload.wikimedia.org/wikipedia/id/e/e5/Menentukan_Arah.jpg',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 8,
+    namaBand: 'Sheila On 7',
+    judulLagu: 'Pemuja Rahasia',
+    photo: 'https://upload.wikimedia.org/wikipedia/id/e/e5/Menentukan_Arah.jpg',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 9,
+    namaBand: 'Kotak',
+    judulLagu: 'Pelan-Pelan Saja',
+    photo: 'https://pelopor.id/wp-content/uploads/2022/06/kotak-800x450.jpg',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+  LirikLagu(
+    id: 10,
+    namaBand: 'Kotak',
+    judulLagu: 'Kecuali Kamu',
+    photo: 'https://pelopor.id/wp-content/uploads/2022/06/kotak-800x450.jpg',
+    lirikLagu: '''
+      lirikLagu
+    ''',
+  ),
+];
