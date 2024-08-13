d95974955a5d40260dc1fba6a59a422bab26fd74
Andri Fajar Sunandhar <andrirrf7@gmail.com>
2024-08-13 15:53:12 +0700

    detail page and button add

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lirik_lagu_app/models/lirik_lagu.dart';
+import 'package:flutter_lirik_lagu_app/pages/detail_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,17 +13,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Lirik Lagu',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text(
+          'Lirik Lagu',
+          style: TextStyle(color: Colors.white),
         ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return Card(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailPage(
+                  lirikLagu: listLirikLagu[index],
+                );
+              }));
+            },
+            child: Card(
               child: ListTile(
                 title: Text(
                   listLirikLagu[index].judulLagu,
@@ -38,9 +47,50 @@ class _HomePageState extends State<HomePage> {
                   fit: BoxFit.cover,
                 ),
               ),
-            );
-          },
-          itemCount: listLirikLagu.length,
-        ));
+            ),
+          );
+        },
+        itemCount: listLirikLagu.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Tambah Lagu'),
+                  content: const Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Nama Lagu'),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Nama Band'),
+                      ),
+                    ],
+                  ),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Create'),
+                    ),
+                  ],
+                );
+              });
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
